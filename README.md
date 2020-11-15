fd44364fe142d824a2fe382530aaf9d3f324d0f1
nadjaharold <rus0clov@gmail.com>
2020-11-15 18:35:37 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# node-api
+
+## development
+
+### Install tools
+
+Please download Docker.
+[https://www.docker.com/](https://www.docker.com/)
+
+```shell
+$ docker -v
+Docker version 19.03.13, build 4484c46d9d
+
+$ docker-compose -v
+docker-compose version 1.27.4, build 40524192
+```
+
+### Download dependencies
+
+```shell
+$ git clone https://github.com/nadjaharold/node-api.git
+$ cd node-api
+```
+
+### Docker build
+
+```shell
+$ docker-compose build
+```
+
+### Docker Container Start
+
+```shell
+$ docker-compose up -d
+```
+
+You can access the following.
+
+[http://localhost:5000/](http://localhost:5000/)
+
+### Reference
+
+[https://serip39.hatenablog.com/entry/2020/06/13/170000](https://serip39.hatenablog.com/entry/2020/06/13/170000)
---
+++ app/client/pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        node-api
+      </h1>
+      <div class="main">
+        <h2>鉄道運行状況</h2>
+        <p class="time">更新日時：{{ data.datetime }}</p>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>路線名</th>
+              <th>運行状況</th>
+              <th>備考</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="train in data.trains" :key="train.yahooId">
+              <td>{{ train.name }}</td>
+              <td>{{ train.status }}</td>
+              <td>{{ train.desc }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <button @click="reload">更新</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  async asyncData({ $axios }) {
+    const data = await $axios.$get("/api/scraping");
+    return { data };
+  },
+  components: {
+    Logo,
+  },
+
+  data: () => ({
+    data: [],
+  }),
+
+  methods: {
+    reload() {
+      location.reload();
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 50px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+h2 {
+  padding: 10px 0;
+}
+.time {
+  text-align: right;
+}
+table {
+  border-collapse: collapse;
+}
+td {
+  padding: 10px;
+}
+button {
+  margin-top: 20px;
+  width: 100%;
+}
+</style>
